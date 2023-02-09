249ecf7a1ba0037d596ace00693df2c726049ee9
asya-code <77514066+asya-code@users.noreply.github.com>
2023-02-09 13:17:52 -0800

    backed is fixed by deleting mvc dependency, postman tested

--- src/main/java/com/mydojo/configuration/Config.java
+++ src/main/java/com/mydojo/configuration/Config.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class Config {
